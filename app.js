d2e21c2bf33d5e1ceee0508202dbc2ba4fd89dbc
logan preston <personal@loganpreston.com>
2020-12-28 17:06:32 -0600

    refactor to use route chaining for get post delete

--- app.js
+++ app.js
@@ -22,40 +22,44 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
-  Article.find({}, function (err, results) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(results);
-    }
-  });
-});
+//refactored to use route chaining instead of discrete routes
+app.route("/articles"
 
-app.post("/articles", function (req, res) {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  newArticle.save(function (err) {
-    if(err){
-        res.send(err);
-    } else{
-        res.send("Success");
-    }
-  });
-});
-
-app.delete("/articles",function(req,res){
-    Article.deleteMany({},function(err,results){
-        if(err){
-            res.send(err)
-        } else{
-            
-            res.send("Successfully deleted all articles")
+    .get(function (req, res) {
+      Article.find({}, function (err, results) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(results);
         }
+      });
     })
-});
+
+    .post(function (req, res) {
+      const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content,
+      });
+      newArticle.save(function (err) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send("Success");
+        }
+      });
+    })
+
+    .delete(function (req, res) {
+      Article.deleteMany({}, function (err, results) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send("Successfully deleted all articles");
+        }
+      });
+    })
+
+);
 
 //listen on to port 3000
 app.listen(3000, function () {
