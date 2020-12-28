d8838e402d423c41055131bb89479c2e40e94968
logan preston <personal@loganpreston.com>
2020-12-28 17:57:39 -0600

    update to target specific articles with get, put, patch, delete

--- app.js
+++ app.js
@@ -23,43 +23,114 @@ const articleSchema = new mongoose.Schema({
 const Article = mongoose.model("Article", articleSchema);
 
 //refactored to use route chaining instead of discrete routes
-app.route("/articles"
+//***REQUESTS TARGETING ALL ARTICLES***//
+app
+  .route("/articles")
 
-    .get(function (req, res) {
-      Article.find({}, function (err, results) {
+  .get(function (req, res) {
+    Article.find({}, function (err, results) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    });
+  })
+
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Success");
+      }
+    });
+  })
+
+  .delete(function (req, res) {
+    Article.deleteMany({}, function (err, results) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully deleted all articles");
+      }
+    });
+  });
+
+//***REQUESTS TARGETING SPECIFIC ARTICLES***//
+app
+  .route("/articles/:articleName")
+
+  .get(function (req, res) {
+    Article.findOne({ title: req.params.articleName }, function (err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        if (result) {
+          res.send(result);
+        } else {
+          res.send("No articles matching that title found.");
+        }
+      }
+    });
+  })
+
+  .put(function (req, res) {
+    Article.updateOne(
+      { title: req.params.articleName },
+      { title: req.body.title, content: req.body.content },
+      function (err, result) {
         if (err) {
           res.send(err);
         } else {
-          res.send(results);
+          if (result.nModified != 0) {
+            res.send("Article updated successfully");
+          } else {
+            res.send("No articles matching that title found.");
+          }
         }
-      });
-    })
+      }
+    );
+  })
 
-    .post(function (req, res) {
-      const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content,
-      });
-      newArticle.save(function (err) {
+  .patch(function (req, res) {
+    Article.updateOne(
+      { title: req.params.articleName },
+      { $set: req.body },
+      function (err, result) {
         if (err) {
           res.send(err);
         } else {
-          res.send("Success");
+          if (result.nModified != 0) {
+            res.send("Article updated successfully");
+          } else {
+            res.send("No articles matching that title found.");
+          }
         }
-      });
-    })
+      }
+    );
+  })
 
-    .delete(function (req, res) {
-      Article.deleteMany({}, function (err, results) {
+  .delete(function (req, res) {
+    Article.deleteOne(
+      { title: req.params.articleName },
+      function (err, result) {
         if (err) {
           res.send(err);
         } else {
-          res.send("Successfully deleted all articles");
+          if (result) {
+            res.send("Successfully deleted the corresponding article");
+          } else {
+            res.send("No article matching that title found.");
+          }
         }
-      });
-    })
-
-);
+      }
+    );
+  });
 
 //listen on to port 3000
 app.listen(3000, function () {
